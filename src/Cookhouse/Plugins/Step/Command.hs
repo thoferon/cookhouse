62608316860da4051506ed6f4ebfaf1afb66a479
thomas.feron <thomas.feron@redspline.com>
2017-02-28 08:48:35 +0000

    Plugin command

---
+++ src/Cookhouse/Plugins/Step/Command.hs
@@ -0,0 +1,36 @@
+module Cookhouse.Plugins.Step.Command
+  ( plugin
+  ) where
+
+import Control.Monad.Trans
+
+import System.Exit
+import System.IO
+import System.Process hiding (runCommand)
+
+import Cookhouse.Plugins.Types
+
+plugin :: StepPlugin
+plugin = StepPlugin
+  { stepPluginName     = "command"
+  , stepPluginRun      = runCommand
+  , stepPluginRollback = emptyRollback
+  }
+
+runCommand :: FilePath -> [(String, String)] -> Handle -> PluginConfig
+        -> PluginM Bool
+runCommand dir envVars handle config = do
+  cmd <- getConfigString config "command"
+
+  let cp  = (shell cmd)
+              { cwd     = Just dir
+              , env     = Just envVars
+              , std_in  = NoStream
+              , std_out = UseHandle handle
+              , std_err = UseHandle handle
+              }
+
+  code <- liftIO $ do
+    (_,_,_,ph) <- createProcess cp
+    waitForProcess ph
+  return $ code == ExitSuccess
