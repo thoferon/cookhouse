2e018ab7b2471897bd88be177b3390d1d01fd32f
thomas.feron <thomas.feron@redspline.com>
2017-04-19 14:23:33 +0000

    The plugin "command" now supports both run/rollback commands

--- src/Cookhouse/Plugins/Step/Command.hs
+++ src/Cookhouse/Plugins/Step/Command.hs
@@ -13,24 +13,25 @@ import Cookhouse.Plugins.Types
 plugin :: StepPlugin
 plugin = StepPlugin
   { stepPluginName     = "command"
-  , stepPluginRun      = runCommand
-  , stepPluginRollback = emptyRollback
+  , stepPluginRun      = runCommand "command"
+  , stepPluginRollback = runCommand "rollback-command"
   }
 
-runCommand :: FilePath -> [(String, String)] -> Handle -> PluginConfig
-        -> PluginM Bool
-runCommand dir envVars handle config = do
-  cmd <- getConfigString config "command"
+runCommand :: String -> FilePath -> [(String, String)] -> Handle -> PluginConfig
+           -> PluginM Bool
+runCommand name dir envVars handle config =
+  case lookupConfigString config name of
+    Nothing  -> return True
+    Just cmd -> do
+      let cp = (shell cmd)
+                 { cwd     = Just dir
+                 , env     = Just envVars
+                 , std_in  = NoStream
+                 , std_out = UseHandle handle
+                 , std_err = UseHandle handle
+                 }
 
-  let cp = (shell cmd)
-             { cwd     = Just dir
-             , env     = Just envVars
-             , std_in  = NoStream
-             , std_out = UseHandle handle
-             , std_err = UseHandle handle
-             }
-
-  code <- liftIO $ do
-    (_,_,_,ph) <- createProcess cp
-    waitForProcess ph
-  return $ code == ExitSuccess
+      code <- liftIO $ do
+        (_,_,_,ph) <- createProcess cp
+        waitForProcess ph
+      return $ code == ExitSuccess
